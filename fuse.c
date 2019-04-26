dc92f3fa0f2b6c11677eccb4308dcd484c3643f7
bayulaxana <bynd4spd@gmail.com>
2019-04-27 04:07:40 +0700

    progress

--- fuse.c
+++ fuse.c
@@ -8,7 +8,12 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
 #define KEY 17
+#define TRUE 1
+#define FALSE 0
 
 static const char *dirpath = "/home/bayulaxana/shift4";
 char *chyper = "qE1~ YMUR2\"`hNIdPzi%^t@(Ao:=CQ,nx4S[7mHFye#aT6+v)DfKL$r?bkOGB>}!9_wV']jcp5JZ&Xl|\\8s;g<{3.u*W-0";
@@ -41,30 +46,150 @@ void decode(char *str) {
     }
 }
 
-// static int xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi)
-// {
-// 	int fd;
+static int xmp_flush(const char *path, struct fuse_file_info *fi)
+{
+	int res;
 
-// 	fd = open(path, fi->flags, mode);
-// 	if (fd == -1)
-// 		return -errno;
+	(void) path;
+	/* This is called from every close on an open file, so call the
+	   close on the underlying filesystem.	But since flush may be
+	   called multiple times for an open file, this must not really
+	   close the file.  This is important if used on a network
+	   filesystem like NFS which flush the data/metadata on close() */
+	res = close(dup(fi->fh));
+	if (res == -1)
+		return -errno;
 
-// 	fi->fh = fd;
-// 	return 0;
-// }
+	return 0;
+}
 
-// static int xmp_write(const char *path, const char *buf, size_t size,
-// 		     off_t offset, struct fuse_file_info *fi)
-// {
-// 	int res;
+static int xmp_truncate(const char *path, off_t size)
+{
+	int res;
+	char new_name[1221], fpath[1221];
+	sprintf(new_name, "%s", path);
+	encode(new_name);
+	sprintf(fpath, "%s%s",dirpath,new_name);
 
-// 	(void) path;
-// 	res = pwrite(fi->fh, buf, size, offset);
-// 	if (res == -1)
-// 		res = -errno;
+	res = truncate(fpath, size);
+	if (res == -1)
+		return -errno;
 
-// 	return res;
-// }
+	return 0;
+}
+
+static int xmp_utimens(const char *path, const struct timespec ts[2])
+{
+	int res;
+	char new_name[1221], fpath[1221];
+	sprintf(new_name, "%s", path);
+	encode(new_name);
+	sprintf(fpath, "%s%s",dirpath,new_name);
+
+	/* don't use utime/utimes since they follow symlinks */
+	res = utimensat(0, fpath, ts, AT_SYMLINK_NOFOLLOW);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_chmod(const char *path, mode_t mode)
+{
+	int res;
+	char new_name[1221], fpath[1221];
+	sprintf(new_name, "%s", path);
+	encode(new_name);
+	sprintf(fpath, "%s%s",dirpath,new_name);
+
+
+	res = chmod(fpath, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_chown(const char *path, uid_t uid, gid_t gid)
+{
+	int res;
+	char new_name[1221], fpath[1221];
+	sprintf(new_name, "%s", path);
+	encode(new_name);
+	sprintf(fpath, "%s%s",dirpath,new_name);
+
+	res = lchown(fpath, uid, gid);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_rename(const char *from, const char *to, unsigned int flags)
+{
+	
+	char n_from[1221], n_to[1221];
+	char source_name[1221], dest_name[1221];
+	int res;
+
+	sprintf(n_from, "%s", from);
+	sprintf(n_to, "%s", to);
+
+	encode(n_from);
+	encode(n_to);
+
+	sprintf(source_name, "%s%s", dirpath,n_from);
+	sprintf(dest_name, "%s%s", dirpath, n_to);
+
+	/* When we have renameat2() in libc, then we can implement flags */
+	if (flags)
+		return -EINVAL;
+
+	res = rename(source_name, dest_name);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+	int fd;
+	char new_name[1221], fpath[1221];
+	sprintf(new_name, "%s", path);
+	encode(new_name);
+	sprintf(fpath, "%s%s",dirpath,new_name);
+
+	fd = open(fpath, fi->flags, mode);
+	if (fd == -1)
+		return -errno;
+
+	fi->fh = fd;
+	return 0;
+}
+
+static int xmp_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+    char fpath[1000], name[1000];
+	sprintf(name,"%s",path);
+    encode(name);
+	sprintf(fpath, "%s%s",dirpath,name);
+
+	(void) fi;
+	fd = open(fpath, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
 
 static int xmp_mkdir(const char *path, mode_t mode)
 {
@@ -126,15 +251,64 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		return -errno;
 
 	while ((de = readdir(dp)) != NULL) {
-		char tmp[10000];
+		char tmp[11511]; // Used to store the decoded string
+		char tmp2[11511]; // Used to 
         struct stat st;
+		struct stat tmstat;
+		/* --------------------------------- */
 		memset(&st, 0, sizeof(st));
 		st.st_ino = de->d_ino;
 		st.st_mode = de->d_type << 12;
-        strcpy(tmp, de->d_name);
+        
+		strcpy(tmp, de->d_name);
+		sprintf(tmp2, "%s/%s", fpath,tmp);
         decode(tmp);
-		res = (filler(buf, tmp, &st, 0));
+
+		/* Nomor 3 --> Starting to check for username chipset or ic_controller */
+
+		stat(tmp2, &tmstat);
+		struct passwd *username = getpwuid(tmstat.st_uid);
+		struct group *grup = getgrgid(tmstat.st_gid);
+
+		printf("%s %s\n",username->pw_name, grup->gr_name);
+
+		int isUsernameChipset = strcmp(username->pw_name, "chipset");
+		int isUsernameIc = strcmp(username->pw_name, "ic_controller");
+		int checkGroup = strcmp(grup->gr_name, "rusak");
+
+		isUsernameChipset = (isUsernameChipset==0 ? TRUE : FALSE);
+		isUsernameIc = (isUsernameIc==0 ? TRUE : FALSE);
+		checkGroup = (checkGroup==0 ? TRUE : FALSE);
+
+		FILE *f_pointer;
+		char to_miris[1221], time_str[1221], buffer[1221];
+
+		if ((isUsernameChipset==TRUE || isUsernameIc==TRUE) && checkGroup==TRUE && !(tmstat.st_mode & 0444))
+		{
+			printf("%d %d %d\n",isUsernameChipset, isUsernameIc, checkGroup);
+			char filemiris[1000];
+
+			strcpy(to_miris, dirpath);
+			strcat(filemiris, "/filemiris.txt");
+			encode(filemiris);
+			
+			strcat(to_miris, filemiris);
+
+			f_pointer = fopen(to_miris, "a+"); // a+ untuk append //
+			time_t now_time = time(NULL);
+			
+
+			strftime(time_str, 50, "%H:%M:%S %D-%m-%Y", localtime(&tmstat.st_atime));
+			fprintf(f_pointer, "%s\t%d:%d\t%s\t%s\n", time_str, username->pw_uid, grup->gr_gid, path, tmp);
+			fclose(f_pointer);
+			remove(tmp2);
+			
+		}
+		else {
+			res = (filler(buf, tmp, &st, 0));
 			if(res!=0) break;
+		}
+		
 	}
 
 	closedir(dp);
@@ -173,8 +347,14 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 }
 
 static struct fuse_operations xmp_oper = {
-	// .create		= xmp_create,
-	// .write		= xmp_write,
+	.flush		= xmp_flush,
+	.truncate	= xmp_truncate,
+	.utimens	= xmp_utimens,
+	.chmod		= xmp_chmod,
+	.chown		= xmp_chown,
+	.rename		= xmp_rename,
+	.create		= xmp_create,
+	.write		= xmp_write,
 	.mkdir		= xmp_mkdir,
 	.getattr	= xmp_getattr,
 	.readdir	= xmp_readdir,
